a7c099f914b5a1a2acd37d8e0067b0e91673e769
Shikha Suman <67411985+Shikha219@users.noreply.github.com>
2021-11-28 14:34:52 +0530

    added models and routes

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,43 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors')
+
+require('dotenv/config');
+
+app.use(cors());
+app.options('*',cors())
+
+//middleware
+app.use(express.json());
+app.use(morgan('tiny'));
 
 
-app.get('/',(req,res)=>{
-    res.send('Hello API!')
+//Routes
+const productsRoutes = require('./routers/products')
+const categoriesRoutes = require('./routers/categories')
+const ordersRouter = require('./routers/orders')
+const usersRouter = require('./routers/users')
+
+
+const api = process.env.API_URL;
+
+//routers
+app.use(`${api}/products`,productsRoutes)
+app.use(`${api}/categories`,categoriesRoutes)
+app.use(`${api}/orders`,ordersRouter)
+app.use(`${api}/users`,usersRouter)
+
+
+
+// Database Connection
+mongoose.connect(process.env.CONNECTION_STRING)
+.then(()=>{
+    console.log('Database Connection is ready...')
+})
+.catch((err)=>{
+    console.log(err)
 })
 
 app.listen(3000, ()=>{
